0f06ea8f65a1d3d2249000ff4285f6b6d47c8877
Mike Morris <eight20eight@gmail.com>
2021-01-13 12:29:43 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using JurassicPark.Models;
+using System.Linq;
+
+namespace JurassicPark
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+
+    }
+  }
+}
