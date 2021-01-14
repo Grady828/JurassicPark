8a41f9bb4793aa90c60bbea0f7c83dbea0a9dd27
Mike Morris <eight20eight@gmail.com>
2021-01-13 23:18:04 -0500

    prompts for the details of a new dino

--- Program.cs
+++ Program.cs
@@ -61,15 +61,7 @@ namespace JurassicPark
                 EnclosureNumber = 13,
             }
             };
-
-            //       -When the app runs it should let the user choose one of the following options.
-
-            // Welcome User, please choose from the following
-
-            //while the user hasn't chosen to quit, keep a loop going until user quits
-            // Algorithm : Before the loop set our boolean to false : they havent quit
             var userHasChosenToQuit = false;
-            // While our boolean is false: show the menu
             while (userHasChosenToQuit == false)
             {
 
@@ -88,30 +80,30 @@ namespace JurassicPark
                 string v = choice.ToUpper().Trim();
                 choice = v;
 
-
-                // This command will show the all the dinosaurs in the list, ordered by WhenAcquired. 
-                // If there aren't any dinosaurs in the park then print out a message that there aren't any.
-                // For every dino do the following: Print name, dietType, whenAquired, weight, enclosureNumber
-
-                foreach (var dino in dinos)
-                    Console.WriteLine($"{dino.Name} is a {dino.DietType} that weighs {dino.Weight}lbs, we got them {dino.WhenAcquired} and they live in display # {dino.EnclosureNumber}");
-
-                if (choice == ADD)
+                if (choice == "VIEW")
+                {
+                    foreach (var dino in dinos)
+                    {
+                        Console.WriteLine($"{dino.Name} is a {dino.DietType} that weighs {dino.Weight}lbs, we got them {dino.WhenAcquired} and they live in display # {dino.EnclosureNumber}");
+                    }
+                }
+                if (choice == "ADD")
+                {
                     //ask name,   
                     Console.WriteLine("What is the name? ");
-                var newDinoName = Console.ReadLine();
+                    var newDinoName = Console.ReadLine();
 
-                // Diet
-                Console.WriteLine("What is it's diet? ");
-                var newDinoDiet = Console.ReadLine();
-                //     weight
-                Console.WriteLine("What is its weight?");
-                var newDinoWeight = int.Parse(Console.ReadLine());
+                    // Diet
+                    Console.WriteLine("What is it's diet? ");
+                    var newDinoDiet = Console.ReadLine();
+                    //     weight
+                    Console.WriteLine("What is its weight?");
+                    var newDinoWeight = int.Parse(Console.ReadLine());
 
-                //     enclosure number
-                Console.WriteLine("What is it's enclosure Number");
-                var newDinoEnclosureNumber = int.Parse(Console.ReadLine());
-
+                    //     enclosure number
+                    Console.WriteLine("What is it's enclosure Number");
+                    var newDinoEnclosureNumber = int.Parse(Console.ReadLine());
+                }
 
                 if (choice == "QUIT")
                 {
@@ -136,8 +128,8 @@ namespace JurassicPark
             // This command will prompt the user for a dinosaur name and a new EnclosureNumber and update that dino's information.
             // Summary
             // This command will display the number of carnivores and the number of herbivores.
-            // Quit
-            // This will stop the program
+
+
 
 
         }
